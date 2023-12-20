edece75a3b81defd07353da3f0451fc4699fa4b8
Shamshieva <burulaiurbaeva7@gmail.com>
2023-12-20 12:48:30 +0600

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BurulaiComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BurulaiComponent from './components/BurulaiComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BurulaiComponent
   }
 }
 </script>
